a1446cef06bf0704ce4562975f22d4d26c57c4ed
Zoe McCarthy <zoemccarthy12@gmail.com>
2023-06-27 08:28:38 -0500

    Update point3D.jl
    Simplified get_x, get_y, get_z, get_w in point3D

--- src/point3D.jl
+++ src/point3D.jl
@@ -14,9 +14,9 @@ end
 
 internal_vec(p::Point3D) = p.vec
 
-get_x(p::Point3D) = internal_vec(p)[1]
-get_y(p::Point3D) = internal_vec(p)[2]
-get_z(p::Point3D) = internal_vec(p)[3]
-get_w(p::Point3D) = internal_vec(p)[4]
+get_x(p::Point3D) = p[1]
+get_y(p::Point3D) = p[2]
+get_z(p::Point3D) = p[3]
+get_w(p::Point3D) = p[4]
 
 
