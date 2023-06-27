9dc115b3a9a82309ceb8f9a692efaaf64dcdf350
Zoe McCarthy <zoemccarthy12@gmail.com>
2023-06-27 08:34:04 -0500

    Update vector4D.jl
    simplified Vector4D implementation

--- src/vector4D.jl
+++ src/vector4D.jl
@@ -16,26 +16,21 @@ end
 
 internal_vec(v::Vector4D) = v.vec
 
-get_x(v::Vector4D) = internal_vec(v)[1]
-get_y(v::Vector4D) = internal_vec(v)[2]
-get_z(v::Vector4D) = internal_vec(v)[3]
-get_w(v::Vector4D) = internal_vec(v)[4]
-
-Vector4D(el::AbstractPGA3Element{T}) where {T<:Real} = Base.convert(Vector4D{T}, el)
+get_x(v::Vector4D) = v[1]
+get_y(v::Vector4D) = v[2]
+get_z(v::Vector4D) = v[3]
+get_w(v::Vector4D) = v[4]
 
 # Define the basic arithmetic operations for Vector4D
 # I think this interface prevents broadcast fusion from happening but we can fix that later if we need the extra performance.
-+(v::Vector4D, w::Vector4D) = Vector4D((v.vec .+ w.vec)...)
--(v::Vector4D, w::Vector4D) = Vector4D((v.vec .- w.vec)...)
-⋅(v::Vector4D, w::Vector4D) = sum(v.vec .* w.vec)
-dot(v::Vector4D, w::Vector4D) = v ⋅ w
++(v::Vector4D, w::Vector4D) = Vector4D(v.vec .+ w.vec)
+-(v::Vector4D, w::Vector4D) = Vector4D(v.vec .- w.vec)
+⋅(v::Vector4D, w::Vector4D) = v.vec ⋅ w.vec
 
+Vector4D(el::AbstractPGA3Element{T}) where {T<:Real} = convert(Vector4D{T}, el)
 
 bulk_norm(v::Vector4D) = norm((internal_vec(v)[1:3]))
 weight_norm(v::Vector4D) = abs(get_w(v))
-
-unitize(v::Vector4D{T}) where {T<:Real} = Vector4D(internal_vec(v) .* (1 / get_w(v)))
-#unitize(v::Vector4D{T}) where {T<:Real} = unitize{Vector4D{T}}(v)
-#unitize{Point3D{T}}(v::Vector4D{T}) where {T<:Real} = Point3D(internal_vec(v) .* (1 / get_w(v)))
+unitize(v::Vector4D) = Vector4D(internal_vec(v) .* (1 / get_w(v)))
 
 
