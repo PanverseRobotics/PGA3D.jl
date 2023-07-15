7975ea06689f092a7e5d955df525bde4a20d1d32
Zoe McCarthy <zoemccarthy12@gmail.com>
2023-07-14 17:49:55 -0700

    disabling the "default error conversion" for now

--- src/conversions.jl
+++ src/conversions.jl
@@ -1,7 +1,7 @@
 # by default, disallow conversions between types unless we explicitly allow them
-function Base.convert(T_to::Type{<:AbstractPGA3DElement}, from::T_from) where {T_from<:AbstractPGA3DElement}
-    throw(error("Conversion not implemented from $(T_from) to $(T_to)."))
-end
+#function Base.convert(T_to::Type{<:AbstractPGA3DElement}, from::T_from) where {T_from<:AbstractPGA3DElement}
+#throw(error("Conversion not implemented from $(T_from) to $(T_to)."))
+#end
 
 # convert a Vector3D to a Vector4D with a 0 in the w component
 Base.convert(::Type{<:Vector4D{T}}, v::AbstractVector3D{T}) where {T<:Real} = Vector4D(internal_vec(v)..., zero(T))
